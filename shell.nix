710ed3f8a29ead3481616c2bfece6109de3b0288
Yuan Wang <yuan.wang@workiva.com>
2020-04-03 11:27:56 -0600

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,33 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc882" }:
+let
+  bootstrap = import <nixpkgs> {};
+
+  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    inherit (nixpkgs) rev sha256;
+  };
+
+  pkgs = import src {};
+  myHaskellPackages = pkgs.haskell.packages."${compiler}";
+
+  # myPackages = myHaskellPackages.callCabal2nix "project" ./blog.cabal {};
+  all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
+in
+myHaskellPackages.shellFor {
+  withHoogle = true;
+  packages = p: [];
+  inherit ((import ./pre-commit.nix).pre-commit-check) shellHook;
+  buildInputs = with myHaskellPackages;
+    [
+      hlint
+      ghcid
+      cabal2nix
+      ormolu
+      cabal-install
+      cabal-fmt
+      (all-hies.selection { selector = p: { inherit (p) ghc882; }; })
+    ];
+}
